4314d52de2cfd424f53b0d56058572a8372cb0c7
sharang23 <ssrivastava@openskydata.com>
2026-01-11 12:33:34 +0000

    Updated app.py

--- test.py
+++
@@ -1,9 +0,0 @@
-from ingestion.load_documents import load_documents
-from ingestion.text_splitter import split_documents
-
-docs = load_documents("data/raw_data")
-chunks = split_documents(docs)
-
-print("Sample chunk:")
-print(chunks[0].page_content[:300])
-print(chunks[0].metadata)
